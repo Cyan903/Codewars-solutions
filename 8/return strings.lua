6e446a35d57d98f2b0fe6682387635ae2aab4885
CyanPiano <cyanp0515@gmail.com>
2021-09-01 23:56:49 -0400

    Added 2 8s, sql/lua

---
+++ 8/return strings.lua
@@ -0,0 +1,7 @@
+-- https://www.codewars.com/kata/55a70521798b14d4750000a4
+return {
+    greet = function(name)
+        --       return string.format("Hello, %s how are you doing today?", name)
+        return "Hello, "..name.." how are you doing today?"
+    end
+};
---
+++ 8/return strings.sql
@@ -0,0 +1,2 @@
+-- https://www.codewars.com/kata/55a70521798b14d4750000a4
+SELECT CONCAT('Hello, ', name ,' how are you doing today?') AS greeting FROM person;
